8f9e370d739941bfc7d1e4a18c819273fbe5c311
tracycute <21520255@gm.uit.edu.vn>
2024-07-15 11:27:06 +0700

    Adding Image Support to Image Model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:tittle, :description, :keyword)
+      params.require(:post).permit(:tittle, :description, :keyword, images: [])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord 
 
+    has_many_attached :images
+
     validates :tittle, presence: true, length: { minimum: 5, maximun: 100 }
     validates :description, presence: true, length: { minimum: 5, maximun: 500 }
     validates :keyword, presence: true, length: { minimum: 5, maximun: 100 }
+
 end
